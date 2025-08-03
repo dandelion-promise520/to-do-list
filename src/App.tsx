934d3d9aca45381271bd9091078388bdd3183234
dandeion <1993466159@qq.com>
2025-08-03 12:35:48 +0800

    项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Avatar, Box, Card, Flex, Text, TextField } from "@radix-ui/themes";
+import { useState, type ChangeEvent, type KeyboardEvent } from "react";
+
+const App = () => {
+  const [thing, setThing] = useState<string>();
+
+  const [list, setList] = useState<string[]>(["吃饭", "睡觉"]);
+
+  // Change事件实时绑定输入框的值
+  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
+    setThing(event.target.value);
+  }
+
+  // onKeyDown事件，提交任务时触发
+  function handleKeyDown(event: KeyboardEvent<HTMLInputElement>): void {
+    if (event.code !== "Enter") return;
+    console.log(thing);
+  }
+
+  return (
+    <div className="h-lvh w-full flex items-center justify-center">
+      <div className="w-3xl flex flex-col items-center space-y-6">
+        <TextField.Root
+          placeholder="请输入待办事项"
+          size={"3"}
+          className="w-full"
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        >
+          <TextField.Slot>
+            <MagnifyingGlassIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
+
+        <Box className="w-full">
+          <Card>
+            <Flex gap="3" align="center">
+              <Box>
+                <Text as="div" size="2" weight="bold">
+                  Teodros Girmay
+                </Text>
+                <Text as="div" size="2" color="gray">
+                  Engineering
+                </Text>
+              </Box>
+            </Flex>
+          </Card>
+        </Box>
+      </div>
+    </div>
+  );
+};
+
+export default App;
